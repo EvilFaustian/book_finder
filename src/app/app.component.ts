2f24893669446457239f9ccff1d63ea77b5c1e4d
Alexandru Ungureanu <evilfaustian@gmail.com>
2022-04-19 19:39:36 +0200

    Main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  apiKey: string = "AIzaSyA2_iAbZSBm7NEpdBsUJ-vKuK6uX8JC1MM"
+  searchInput: string = ""
+  searching: boolean = false;
+  loading: boolean = false;
+  searchResult: any;
+
+  
   title = 'book_finder';
+  constructor(public http: HttpClient) {
+    //this.http.get('https://www.googleapis.com/books/v1/volumes?q=flowers&projection=lite&key=' + this.apiKey).subscribe(res => {
+    //  console.log(res)
+    //})
+  }
+
+  search() {
+    let searchTerms = this.searchInput.replace(/\s/g, '+').trim().toLowerCase()
+    if (this.searching) {
+      //Do something if its searching already
+    } else {
+      this.searching = true;
+      this.loading = true;
+      this.searchResult = "";
+      this.http.get(' https://www.googleapis.com/books/v1/volumes?q=' + searchTerms + '&key=' + this.apiKey).subscribe(res => {
+        this.searchResult = res;
+        this.loading = false;
+        console.log("Search Result", this.searchResult);
+        })
+      setTimeout(() => {
+        this.searching = false;
+      }, 500)
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http"
+import {FormsModule} from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
